7db99186f6a4f3fdc03202d5612b983a7f9e6a5f
hyperthreading <no1expman@gmail.com>
2018-01-16 22:10:32 +0900

    Add swiper, persp-mode

--- customizations/navigation.el
+++ customizations/navigation.el
@@ -37,3 +37,15 @@
 
 (global-set-key (kbd "s-w") 'delete-window)
 
+(require 'persp-mode)
+(persp-mode t)
+(with-eval-after-load "persp-mode-projectile-bridge-autoloads"
+  (add-hook 'persp-mode-projectile-bridge-mode-hook
+            #'(lambda ()
+                (if persp-mode-projectile-bridge-mode
+                    (persp-mode-projectile-bridge-find-perspectives-for-all-buffers)
+                  (persp-mode-projectile-bridge-kill-perspectives))))
+  (add-hook 'after-init-hook
+            #'(lambda ()
+                (persp-mode-projectile-bridge-mode 1))
+            t))
--- init.el
+++ init.el
@@ -24,11 +24,16 @@
                      ;; Markdown
                      markdown-mode
 
+                     ;; Project Management
+                     
 		     magit
 		     dired+
 		     projectile
                      helm
                      helm-projectile
+
+                     persp-mode
+                     persp-mode-projectile-bridge
 
                      ;; Editing features
 		     flycheck
@@ -42,7 +47,10 @@
                      lsp-ui
                      lsp-javascript-typescript
                      lsp-python
+                     
                      ace-jump-mode
+                     swiper
+                     swiper-helm
 
                      ;; Python Support
                      ein
@@ -75,7 +83,7 @@
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
    (quote
-    (lsp-ui company-quickhelp undo-tree inf-clojure jedi python-mode ein use-package smartparens rainbow-delimiters flycheck-clojure flycheck-pos-tip ace-jump-mode challenger-deep-theme company-lsp markdown-mode helm-projectile flycheck-ycmd company-ycmd ycmd company flycheck expand-region projectile dired+ magit json-mode rjsx-mode paredit cider))))
+    (persp-mode-projectile-bridge multiple-cursors persp-mode swiper-helm swiper company-jedi lsp-ui company-quickhelp undo-tree inf-clojure jedi python-mode ein use-package smartparens rainbow-delimiters flycheck-clojure flycheck-pos-tip ace-jump-mode challenger-deep-theme company-lsp markdown-mode helm-projectile flycheck-ycmd company-ycmd ycmd company flycheck expand-region projectile dired+ magit json-mode rjsx-mode paredit cider))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
